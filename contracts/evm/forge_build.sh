589dfad870582221b338049e2066b56ffd8fb28c
Deepak Bomjan <dpaktamang@gmail.com>
2024-02-01 08:21:58 +0545

    ci: add solidity release

---
+++ contracts/evm/forge_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+forge build
+mkdir artifacts
+cat out/CallService.sol/CallService.json | jq '{"abi": .abi, "bytecode": .bytecode.object}'  > artifacts/xcall_abi_bytecode.json
+cat out/CentralizedAdapter.sol/XCallCentralizeConnection.json | jq '{"abi": .abi, "bytecode": .bytecode.object}' > artifacts/centralized_connection_abi_byte_code.json
+cat out/LayerZeroAdapter.sol/LayerZeroAdapter.json | jq '{"abi": .abi, "bytecode": .bytecode.object}' > artifacts/layer_zero_adapter_abi_bytecode.json
+cat out/WormholeAdapter.sol/WormholeAdapter.json | jq '{"abi": .abi, "bytecode": .bytecode.object}' > artifacts/wormhole_adapter_abi.json
